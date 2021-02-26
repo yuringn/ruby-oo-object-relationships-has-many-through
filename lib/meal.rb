209ea166e329d73c8f8f4886b0625de3ed240b6d
Yuri Nguyen <yuringuyen312@gmail.com>
2021-02-25 23:11:20 -0500

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,24 @@
+require"pry"
 class Meal
+attr_accessor :waiter, :customer, :total, :tip
+@@all = []
+
+def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+def meals
+    Meal.all.select do |meal|
+        meal.customer == self
+    end
+end
 
 end
